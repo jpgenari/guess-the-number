6f82093ae288a4620adf99bef4166952b173c9ca
Joao Paulo Genari <jpgenari@gmail.com>
2023-07-31 22:57:11 +0100

    Feat: add function to get user level and update instructions function

--- run.py
+++ run.py
@@ -35,12 +35,10 @@ def instructions():
     print()
     print('You can view instructions or just start playing!')
     print()
-    instructions = input('Do you want to check the instructions? Y/N: ').lower()
 
-    while instructions != 'y' and instructions != 'n':
+    while True:
+        instructions = input('Do you want to check the instructions? Y/N: ').lower()
         clear()
-        print('Incorrect entry, you should either pick [Y]es or [N]o')
-    else:
         if instructions == 'y':
             clear()
             print()
@@ -53,7 +51,7 @@ def instructions():
             print()
             print('      Be aware, the fewer incorrect guesses, the higher your score:')
             print('      1st guess: 500 pts, 2nd guess: 300 pts, 3rd guess: 100 pts and')
-            print('             with 4th guess: well, you lose! The poins will')
+            print('             with 4th guess: well, you lose! The points will')
             print('         sum up through the rounds and you can log them at the end!')
             print()
             print('                              Good look!')
@@ -61,8 +59,43 @@ def instructions():
             print()
             print()
             back = input('                       Press ENTER to proceed: ')
+            clear()
+            break
+        elif instructions == 'n':
+            break
+        else:
+            welcome_message()
+            print()
+            print('Incorrect entry, you should either pick [Y]es or [N]o')
+            print()
 
 
+def user_level_choice():
+    '''
+    Provides to the user the game level choice and get
+    user's option. 
+    Try method implemented to validate data: collects int and 
+    raises error if input is not a number and if number
+    is not 1 or 2.
+    '''
+    while True:
+        print()
+        game_level = input('Enter 1 for easy or 2 for hard level: ')
+        clear()
+
+        try:
+            game_level = int(game_level)
+        except ValueError:
+            welcome_message()
+            print('Numbers only!')
+            continue
+        if 1 <= game_level <= 2:
+            return game_level
+            break
+        else:
+            welcome_message()
+            print('Number outside the allowed range.')
+        
 def main():
     '''
     Runs the program outside game loop
@@ -70,5 +103,7 @@ def main():
     clear()
     welcome_message()
     instructions()
+    welcome_message()
+    user_level = user_level_choice()
 
 main()
